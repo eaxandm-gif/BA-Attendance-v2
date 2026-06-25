3feef83cc3993e1603b66a2fec4a64c35855267c
eaxandm-gif <ea.xandm@gmail.com>
2026-06-25 16:53:31 +0700

    Update LIFF_ID in configuration

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BA_CONFIG = Object.freeze({
   VERSION: '2.0.0',
-  LIFF_ID: '2010510038-3cgY3jd4',
+  LIFF_ID: '2010513129-PCvZnSjw',
   SUPABASE_URL: 'https://aohagiyraxolmhnvfgdv.supabase.co',
   EDGE_FUNCTION_URL:
     'https://aohagiyraxolmhnvfgdv.supabase.co/functions/v1/ba-api',
