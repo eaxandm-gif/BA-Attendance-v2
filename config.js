7e985054f73857d28512e35781970342713c8518
eaxandm-gif <ea.xandm@gmail.com>
2026-06-25 15:44:06 +0700

    Replace SUPABASE_ANON_KEY with EDGE_FUNCTION_URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.BA_CONFIG = Object.freeze({
   VERSION: '2.0.0',
   LIFF_ID: '2010510038-3cgY3jd4',
   SUPABASE_URL: 'https://aohagiyraxolmhnvfgdv.supabase.co',
-  SUPABASE_ANON_KEY: 'ce90fd2baa3d5140975e67f4226f6983b1cf259127d047625ee1b3fa874439f5 ',
-  BA_API_URL:
-    'https://aohagiyraxolmhnvfgdv.supabase.co/functions/v1/ba-api'
+  EDGE_FUNCTION_URL:
+    'https://aohagiyraxolmhnvfgdv.supabase.co/functions/v1/ba-api',
+  REQUEST_TIMEOUT_MS: 30000
 });
