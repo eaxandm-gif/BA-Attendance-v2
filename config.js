386e7566c2d6849d5a6d338673ac5ea3aaed10c1
eaxandm-gif <ea.xandm@gmail.com>
2026-06-26 14:15:35 +0700

    Update LIFF_ID in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BA_CONFIG = Object.freeze({
   VERSION: '2.3.1',
-  LIFF_ID: 'ใส่_LIFF_ID_ใหม่_ของคุณตรงนี้',
+  LIFF_ID: '2010513129-PCvZnSjw',
   SUPABASE_URL: 'https://aohagiyraxolmhnvfgdv.supabase.co',
   EDGE_FUNCTION_URL: 'https://aohagiyraxolmhnvfgdv.supabase.co/functions/v1/ba-api',
   REQUEST_TIMEOUT_MS: 30000
